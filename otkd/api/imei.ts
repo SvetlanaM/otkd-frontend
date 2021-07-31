5441a0dd76e8fe84d0e0e736c5adc581c5556531
SvetlanaM <richard.margeta@gmail.com>
2021-07-31 22:23:18 +0200

    Create API call for imei - setup

--- otkd/api/imei.ts
+++ otkd/api/imei.ts
@@ -1,1 +1,28 @@
-import axios from 'axios'
+import axios, {AxiosResponse} from 'axios'
+
+const SERVER_BASE_URL = process.env.SERVER_BASE_URL
+
+const ImeiAPI = {
+	update: async (
+		team_number: number
+	): Promise<Omit<AxiosResponse, 'statusText' | 'headers' | 'config'>> => {
+		const {data, status} = await axios.patch(
+			`${SERVER_BASE_URL}/team/${team_number}/tracker`,
+			JSON.stringify({team_number}),
+			{
+				headers: {
+					'Content-Type': 'application/json; charset=utf-8`',
+					'Authorization': `Bearer ${encodeURIComponent(
+						process.env.AUTH_TOKEN || ''
+					)}`,
+				},
+			}
+		)
+		return {
+			data,
+			status,
+		}
+	},
+}
+
+export default ImeiAPI
