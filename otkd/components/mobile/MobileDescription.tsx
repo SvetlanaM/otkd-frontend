d341a5884c37624eb348c61df2d81359523485db
SvetlanaM <richard.margeta@gmail.com>
2021-08-05 14:54:14 +0200

    Show more update

--- otkd/components/mobile/MobileDescription.tsx
+++ otkd/components/mobile/MobileDescription.tsx
@@ -6,43 +6,41 @@ const MobileDescription = (): JSX.Element => {
 
 	useEffect(() => {
 		ref.current && Number(window.innerWidth) < 780 && setShowMore(false)
-	}, [ref])
+	}, [])
 
-	const [showMore, setShowMore] = useState(true)
+	const [showMore, setShowMore] = useState(false)
 	return (
 		<div
 			className="mt-6 font-lighter leading-1 tracking-wide text-sm text-purple-light"
 			ref={ref}>
 			Dokument je možné nahrať priamo v aplikácii, prípadne sa ním preukázať na
 			štarte alebo poslať na oficiálny email beh345@odtatierkdunaju.sk.
-			{ref.current && Number(window.innerWidth) < 780 && (
-				<a
-					onClick={() => setShowMore(!showMore)}
-					className="w-full focus:outline-none font-semibold">
-					{!showMore ? (
-						<div className="flex justify-between w-full mt-4">
-							<p>Viac informácií</p>
-							<Image
-								src={'/icons/right.svg'}
-								alt="Sipka"
-								width={20}
-								height={20}
-							/>
-						</div>
-					) : (
-						<div className="flex justify-between w-full mt-4">
-							<p>Skryť</p>
-							<Image
-								src={'/icons/right.svg'}
-								alt="Sipka"
-								width={20}
-								height={20}
-								className="rotate-90 transform"
-							/>
-						</div>
-					)}
-				</a>
-			)}
+			<a
+				onClick={() => setShowMore(!showMore)}
+				className="w-full focus:outline-none font-semibold">
+				{!showMore ? (
+					<div className="flex justify-between w-full mt-4">
+						<p>Viac informácií</p>
+						<Image
+							src={'/icons/right.svg'}
+							alt="Sipka"
+							width={20}
+							height={20}
+						/>
+					</div>
+				) : (
+					<div className="flex justify-between w-full mt-4">
+						<p>Skryť</p>
+						<Image
+							src={'/icons/right.svg'}
+							alt="Sipka"
+							width={20}
+							height={20}
+							className="rotate-90 transform"
+						/>
+					</div>
+				)}
+			</a>
 			{showMore && (
 				<>
 					<ol className="mt-3.5">
--- otkd/components/mobile/MobileWrapper.tsx
+++ otkd/components/mobile/MobileWrapper.tsx
@@ -2,7 +2,7 @@ interface MobileWrapperProps {
 	children: React.ReactNode
 }
 const MobileWrapper = ({children}: MobileWrapperProps) => (
-	<div className="bg-blue-dark min-h-screen text-white flex flex-col items-center leading-2 font-light tracking-wide px-8 lg:px-0 justify-start md:justify-center">
+	<div className="bg-blue-dark min-h-screen text-white flex flex-col items-center leading-2 font-light tracking-wide px-8 lg:px-0 justify-center md:justify-center">
 		<section className="w-full md:w-3/4 px-2 md:px-10 mt-12 md:mt-0 mb-12 md:mb-0">
 			{children}
 		</section>
